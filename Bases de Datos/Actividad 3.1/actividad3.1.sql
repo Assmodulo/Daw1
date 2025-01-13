f30c6a30ada8173adf7bed7a2b5ce155c1e74bfb
SOBREMESA_ALEX\Alex <alejandro>
2025-01-13 07:31:24 +0100

    aaa

--- Bases de Datos/Actividad 3.1/actividad3.1.sql
+++ Bases de Datos/Actividad 3.1/actividad3.1.sql
@@ -23,9 +23,9 @@ create table empleados(
     
 create table vehiculo(
 	matricula char(7) unique not null,
-    marca varchar(15) not null,
-    modelo varchar(15) not null,
-    empleado char(9) not null,
+    marca varchar(15),
+    modelo varchar(15),
+    empleado char(9),
     constraint matricula primary key (matricula),
     constraint empleado foreign key (empleado) references empleados(dni)
 );
@@ -34,5 +34,32 @@ insert into departamento (nombre,ubicacion) values('CONTABILIDAD','TERCERA PLANT
 insert into empleados values('13000001A','ANTONIO','GARCIA FERNANDEZ','2008-11-12',1);
 insert into vehiculo values('0000AAA','RENAULT','RAPIDO','13000001A');
 
-insert into empleados (dni, nombre, apellidos, fecha_nacimientos) values ('13000002B','PEPE','RODRIGUEZ','2000-12-12');
-    
+insert into empleados (dni, nombre, apellidos, fecha_nacimiento) values ('13000002B','PEPE','RODRIGUEZ','2000-12-12'),('13000003C','JUAN','DIAZ PEREZ','1995-11-06'),('13000004D','MARIA','GONZALEZ PI','1996-09-07');
+insert into vehiculo (matricula) values ('1234ABC');
+
+#En el enunciado del ejercicio dice cambiar el nombre al empleado numero 2, en nuestro caso, como el numero
+#2 no existe, si no que los identificamos por el dni es aquel cuyo dni sea el 13000002B
+
+update empleados set nombre = 'CARLOS' where dni='13000002B';
+
+#cambiar el nombre del departamento 1, el único que hay, por IT
+
+update departamento set nombre ='IT' where id=1;
+
+#En el mismo caso que cambiar el nombre del empleado, para el vehiculo el where será la matricula
+
+update vehiculo set marca='SEAT', modelo='IBIZA' where matricula='1234ABC';
+
+#Eliminar al empleado numero 4, en este caso por dni 13000004D
+
+delete from empleados where dni='13000004D';
+
+#elimina el coche de empresa numero 2, where, matricula = 1234ABC
+
+delete from vehiculo where matricula='1234ABC';
+
+delete from vehiculo;
+delete from empleados;
+delete from departamento;
+
+
